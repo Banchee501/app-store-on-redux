644a8cd3b1ca72009d288fdc8d3db183ad7c26c1
Banchee501 <rosstactical373@gmail.com>
2023-02-22 21:44:15 +0200

    add Routes

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <main className="app">
-      <div className="content">
-        
+    <Router>
+      <div className="app">
+        <main>
+          <Routes>
+            <Route path="/" element={<MainPage/>}/>
+            <Route exact path="/catalog" element={<CatalogPage/>}/>
+            <Route path="/catalog/:id" element={<Products/>}/>
+            <Route path="/basket" element={<Basket/>}/>
+            <Route exact path="/profile" element={<Profile/>}/>
+            <Route path="/login/profile" element={<SignIn/>}/>
+            <Route path="*" element={<Page404 />} />
+          </Routes>
+        </main>
       </div>
-    </main>
+    </Router>
   );
 }
 
