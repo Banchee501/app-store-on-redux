e1d5381fa71377894bf2b004926ed57a36f3e8c0
Banchee501 <rosstactical373@gmail.com>
2023-02-23 21:36:19 +0200

    create .gitconfig, delete Routes

--- src/components/pages/Navbar.js
+++ src/components/pages/Navbar.js
@@ -1,25 +1,36 @@
+import { NavLink } from "react-router-dom";
 
+import "../../style/style.scss"
 
 const Navbar = () => {
-    <nav>
-        <ul>
-            <li>
+    let activeStyle = {
+        textDecoration: "none",
+      };
 
-            </li>
-            <li>
-
-            </li>
-            <li>
-
-            </li>
-            <li>
-
-            </li>
-            <li>
-
-            </li>
-        </ul>
-    </nav>
+      return (
+        <nav>
+            <ul>
+                <li>
+                    <NavLink to="services" style={({ isActive }) => isActive ? activeStyle : null}>ПОСЛУГИ</NavLink>
+                </li>
+                <li>
+                    <NavLink to="example" style={({ isActive }) => isActive ? activeStyle : null}>ПРИКЛАДИ</NavLink>
+                </li>
+                <li>
+                    <NavLink to="promotion" style={({ isActive }) => isActive ? activeStyle : null}>АКЦІЇ</NavLink>
+                </li>
+                <li>
+                    <NavLink to="catalog" style={({ isActive }) => isActive ? activeStyle : null}>КАТАЛОГ</NavLink>
+                </li>
+                <li>
+                    <NavLink to="reviews" style={({ isActive }) => isActive ? activeStyle : null}>ВІДГУКИ</NavLink>
+                </li>
+                <li>
+                    <NavLink to="contacts" style={({ isActive }) => isActive ? activeStyle : null}>КОНТАКТИ</NavLink>
+                </li>
+            </ul>
+        </nav>
+    )
 }
 
 export default Navbar
