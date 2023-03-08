3d2984bfdc06b25ae1b1a727cffe92b2c70389d6
Banchee501 <rosstactical373@gmail.com>
2023-03-08 14:17:25 +0200

    complete services page

--- src/components/pages/mainPage/MainPage.js
+++ src/components/pages/mainPage/MainPage.js
@@ -3,26 +3,31 @@ import { Link } from "react-router-dom";
 import Header from "../header/Header";
 import Navbar from "../navbar/Navbar";
 
+import Services from "../servicesPage/Services";
+
 import "./mainPage.scss"
 
 const MainPage = () => {
- return (
-    <section className="main">
-        <div className="container">
-            <div className="triangle"></div>
-            <div className="photo"></div>
-            <Header/>
-            <Navbar/>
-            <div className="main-description">
-                <p className="main-description__title">MASONS <br></br><span className="color">BRANDING</span><span className="filter">BRANDING</span> <br></br>WORDSHOP</p>
-                <p className="main-description__subtitle">Високоякісний друк <br></br> Брендування одягу та аксесуарів</p>
-                <button className='button catalog'>
-                    <Link to='/catalog'>КАТАЛОГ</Link>
-                </button>
-            </div>  
-        </div>
-    </section>
- )
+    return (
+        <>
+            <section className="main">
+                <div className="container">
+                    <div className="triangle"></div>
+                    <div className="photo"></div>
+                    <Header/>
+                    <Navbar/>
+                    <div className="main-description">
+                        <p className="main-description__title">MASONS <br></br><span className="color">BRANDING</span><span className="filter">BRANDING</span> <br></br>WORDSHOP</p>
+                        <p className="main-description__subtitle">Високоякісний друк <br></br> Брендування одягу та аксесуарів</p>
+                        <button className='button catalog'>
+                            <Link to='/catalog'>КАТАЛОГ</Link>
+                        </button>
+                    </div>
+                </div>
+            </section>
+            <Services/>
+        </>
+    )
 }
 
 export default MainPage;
